a1bee3b75d0511b7f1bbdce2eee1875133fae1ed
Martin Gillmann <martin.gillmann@canfordsoftware.com>
2017-10-26 07:53:14 -0300

    C# WhatsAppAPI made public.

---
+++ WhatsAppAPI_CS/WhatsAppAPI_CS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+
+namespace WhatsAppAPI_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WhatApp whatsApp = new WhatApp("<Your unique X-APIId>");        // you get this on https://niceapi.net/Details 
+            whatsApp.Send("<Mobile number>", "Sent from my application.");  // one of the numbers you registred at https://niceapi.net/Register
+        }
+    }
+}
---
+++ WhatsAppAPI_CS/WhatsAppAPI_CS/WhatApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+
+namespace WhatsAppAPI_CS
+{
+    class WhatApp
+    {
+        private String APPIID;
+
+        public WhatApp(String appid)
+        {
+            this.APPIID = appid;
+        }
+
+        public bool Send(String mobileNo, String message) 
+        {
+            try
+            {
+                string url = "https://NiceApi.net/API";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Headers.Add("X-APIId", APPIID);
+                request.Headers.Add("X-APIMobile", mobileNo);
+                using (StreamWriter streamOut = new StreamWriter(request.GetRequestStream()))
+                {
+                    streamOut.Write(message);
+                }
+                using (StreamReader streamIn = new StreamReader(request.GetResponse().GetResponseStream()))
+                {
+                    return streamIn.ReadToEnd().Equals("queued");
+                }
+            }
+            catch 
+            {
+            }
+            return false;
+        }
+    }
+}
